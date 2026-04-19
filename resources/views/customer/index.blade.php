21b19d34975d3849921cab4d309c5cc95e777400
Muh Rafli <muhr89890@gmail.com>
2026-04-19 08:08:40 -0700

    feat:implementasi fitur read untuk [Pelanggan]

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -4,7 +4,7 @@
     
     <ul class="list-group">
         @foreach ($customers as $customer )
-        
+    
         <li class="list-group-item">
             {{ $loop->iteration }}. {{ $customer->nama }} || {{ $customer->email }} || {{ $customer->No_telepon }} || {{ $customer->Alamat }} || {{ $customer->pekerjaan }}</li>
         @endforeach

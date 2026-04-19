50b56c3a04b5a09e5ea9be44e473d01cbb313251
Muh Rafli <muhr89890@gmail.com>
2026-04-19 01:07:23 -0700

    feat:penyiapan blade component untuk layout utama

---
+++ resources/views/customer/create.blade.php
@@ -0,0 +1,6 @@
+<x-app>
+
+    <x-slot:title>Create Customer</x-slot>
+ 
+    <h1 class="fw-bold">From Data Customer</h1>
+</x-app>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/customer', function () {
+    return view('customer.index', ['title' => 'Customer']);
+});
+
+Route::get('/customer/create', function () {
+    return view('customer.create', ['title' => 'Customer Create']);
+});
